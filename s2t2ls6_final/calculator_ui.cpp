46b258a216ca8e2c8debd71d70aa0d7b99a5b5e1
gitVictors <WinVictors@mail.ru>
2025-02-16 14:09:15 +0300

    debug sourse

--- s2t2ls6_final/calculator_ui.cpp
+++ s2t2ls6_final/calculator_ui.cpp
@@ -3,20 +3,77 @@
 using namespace std::literals;
 
 bool CalculatorUI::Parse(std::istream& input) {
+
     Number operand;
     if (ReadNumber(input, operand)) {
         calc_.Set(operand);
     }
+ 
+    calc_.Set(operand);
 
-    // Напишите здесь цикл чтения на основе функции RunCalculatorCycle.
+    std::string token;
+    while(input >> token) {
+        Number right;
+        if (token == "+"s) {
+            if (!ReadNumber(input, right)) {
+                break;
+            }
+            calc_.Add(right);
+        } else if (token == "-"s) {
+            if (!ReadNumber(input, right)) {
+                break;
+            }
+            calc_.Sub(right);
+        } else if (token == "*"s) {
+            if (!ReadNumber(input, right)) {
+                break;
+            }
+            calc_.Mul(right);
+        } else if (token == "/"s) {
+            if (!ReadNumber(input, right)) {
+                break;
+            }
+            calc_.Div(right);
+        } else if (token == "**"s) {
+            if (!ReadNumber(input, right)) {
+                break;
+            }
+            calc_.Pow(right);
+        } else if (token == "s"s) {
+            calc_.Save();
+        } else if (token == "l"s) {
+            if (!calc_.HasMem()) {
+                err_ << "Error: Memory is empty"s << std::endl;
+                break;
+            }
+            calc_.Load();
+        } else if (token == "="s) {
+            output_ << calc_.GetNumberRepr() << std::endl;
+        } else if (token == "c"s) {
+            calc_.Set(0);
+        } else if (token == "n"s) {
+            calc_.Set(-calc_.GetNumber());
+        } else if (token == ":"s) {
+            if (!ReadNumber(input, right)) {
+                break;
+            }
+            calc_.Set(right);
+        } else if (token == "q"s) {
+            return true;
+        } else {
+            err_ << "Error: Unknown token "s << token <<std::endl;
+            return false;
+        }
+    }
+    return false;
+
 
     return !input;
 }
 
 bool CalculatorUI::ReadNumber(std::istream& input, Number& result) const {
     if (!(input >> result)) {
-        // Тут нужно вывести сообщение об ошибке в поток,
-        // сохранённый внутри класса.
+        err_ << "Error: Numeric operand expected"s << std::endl;
         return false;
     }
     return true;
--- s2t2ls6_final/calculator_ui.h
+++ s2t2ls6_final/calculator_ui.h
@@ -9,16 +9,27 @@ public:
     // Реализуйте конструктор. Он должен сохранить ссылки 
     // на потоки и калькулятор внутрь класса.
     // Используйте для этого список инициализации.
-    CalculatorUI(Calculator& calc, std::ostream& out, std::ostream& err);
+    CalculatorUI(Calculator& calc, std::ostream& out, std::ostream& err):
+    calc_{calc},
+    output_{out},
+    err_{err}
+    {
+
+    }
     
     // Заготовки следующих трёх методов есть в .cpp-файле.
     // Дополните их.
     bool Parse(std::istream& input);
+
 
 private:
     bool ReadNumber(std::istream& input, Number& result) const;
     void Help() const;
     
 private:
-    // Добавьте сюда необходимые члены-данные.
+    Calculator& calc_;
+    std::ostream& output_;
+    std::ostream& err_;
+    
+
 };
