ed3aa1dfb39aeb3ef22c9d1131c670e42180c24b
sv <zuuuuk@mail.ru>
2025-04-25 17:41:27 +0300

    шаблоны функций. сум, макс, уножения

--- sprint3/Tems16_25Lesson4_9_z/template_sum/operations.h
+++ sprint3/Tems16_25Lesson4_9_z/template_sum/operations.h
@@ -24,11 +24,31 @@ T Prod(T t, S s, U u){
 template<typename T, typename S, typename U>
 T Max(T t, S s, U u){
 
+    // Сравниваем первый и второй аргумент, находим максимум
+    auto max_temp = t > s ? t : s;
+
+    // Сравниваем временный максимум с третьим аргументом
+    return max_temp > u ? max_temp : u;
+
 }
 
 // Вызов указанной функции для параметров t, s, u.
 template<typename T, typename S, typename U>
-T DoOperation(T t, S s, U u, OperationType operation);
+T DoOperation(T t, S s, U u, OperationType operation){
+
+    T res{0};
+
+    if (operation == OperationType::OP_SUM)
+        return Sum(t, s, u);
+
+    if (operation == OperationType::OP_PROD)
+        return Prod(t, s, u);
+
+    if (operation == OperationType::OP_MAX)
+        return Max(t,s,u);
+
+    return res;
+}
 
 //  Cпециализация Prod для строк.
 template<>
