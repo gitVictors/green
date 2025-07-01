fd50978d570220ede36c2d514f532bb3e753bb89
gitVictors <WinVictors@mail.ru>
2025-07-01 22:25:57 +0300

    hash машинных номеров

---
+++ sprint9/Tems1_16lesson3_10/conteyner/main.cpp
@@ -0,0 +1,136 @@
+#include <array>
+#include <iomanip>
+#include <iostream>
+// #include <optional>
+#include <sstream>
+#include <string>
+ #include <vector>
+#include <algorithm>
+#include <unordered_set>
+
+using namespace std;
+
+class VehiclePlate {
+public:
+    VehiclePlate(char l0, char l1, int digits, char l2, int region)
+        : letters_{l0, l1, l2}
+        , digits_(digits)
+        , region_(region) {
+    }
+
+    string ToString() const {
+        ostringstream out;
+        out << letters_[0] << letters_[1];
+        // чтобы дополнить цифровую часть номера слева нулями
+        // до трёх цифр, используем подобные манипуляторы:
+        // setfill задаёт символ для заполнения,
+        // right задаёт выравнивание по правому краю,
+        // setw задаёт минимальное желаемое количество знаков
+        out << setfill('0') << right << setw(3) << digits_;
+        out << letters_[2] << setw(2) << region_;
+
+        return out.str();
+    }
+
+    int Hash() const {
+        // Преобразуем массив букв в числовое значение
+        int letters_hash = 0;
+        for (char letter : letters_) {
+            letters_hash = letters_hash * 128 + letter;
+        }
+
+        // Объединяем все значения в одно хеш-значение
+        return letters_hash ^ digits_ ^ region_;
+    }
+
+    bool operator==(const VehiclePlate& other) const {
+        return letters_ == other.letters_ && digits_ == other.digits_ && region_ == other.region_;
+    }
+
+
+private:
+    array<char, 3> letters_;
+    int digits_;
+    int region_;
+};
+
+ostream& operator<<(ostream& out, VehiclePlate plate) {
+    out << plate.ToString();
+    return out;
+}
+
+template <typename T>
+class HashableContainer {
+public:
+
+    void Insert(T elem) {
+        int index = elem.Hash();
+
+        // если вектор недостаточно велик для этого индекса,
+        // то увеличим его, выделив место с запасом
+        if (index >= int(elements_.size())) {
+            elements_.resize(index * 2 + 1);
+        }
+
+        auto& elm = elements_[index]; //.push_bask (elem); //= move(elem);
+        if (std::find(elm.begin(), elm.end(), elem  ) == elm.end()){
+            elm.push_back(elem);
+        }
+    }
+
+    // void Insert(const T& elem) {
+    //     size_t index = elem.Hash() % elements_.size();
+
+    //     // Если вектор пуст, инициализируем его
+    //     if (elements_.empty()) {
+    //         elements_.resize(1);
+    //         index = 0;
+    //     }
+    //     // Если индекс выходит за границы, увеличиваем размер
+    //     else if (index >= elements_.size()) {
+    //         elements_.resize(index + 1);
+    //     }
+
+    //     // Вставка в unordered_set (автоматически проверяет дубликаты)
+    //     elements_[index].insert(elem);
+    // }
+
+
+
+    void PrintAll(ostream& out) const {
+        for (const auto& bag : elements_) {
+            for (const auto& e : bag) {
+                out << e << endl;
+            }
+        }
+    }
+
+
+    const auto& GetVector() const {
+        return elements_;
+    }
+
+private:
+    // vector<optional<T>> elements_;
+    vector<vector<T>> elements_;
+   //  std::vector<std::unordered_set<T>> elements_;
+};
+
+int main() {
+    HashableContainer<VehiclePlate> plate_base;
+    plate_base.Insert({'B', 'H', 840, 'E', 99});
+    plate_base.Insert({'O', 'K', 942, 'K', 78});
+    plate_base.Insert({'O', 'K', 942, 'K', 78});
+    plate_base.Insert({'O', 'K', 942, 'K', 78});
+    plate_base.Insert({'O', 'K', 942, 'K', 78});
+    plate_base.Insert({'H', 'E', 968, 'C', 79});
+    plate_base.Insert({'T', 'A', 326, 'X', 83});
+    plate_base.Insert({'H', 'H', 831, 'P', 116});
+    plate_base.Insert({'P', 'M', 884, 'K', 23});
+    plate_base.Insert({'O', 'C', 34, 'P', 24});
+    plate_base.Insert({'M', 'Y', 831, 'M', 43});
+    plate_base.Insert({'K', 'T', 478, 'P', 49});
+    plate_base.Insert({'X', 'P', 850, 'A', 50});
+
+    plate_base.PrintAll(cout);
+}
