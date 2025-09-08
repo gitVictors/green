3263116f098705774fda2bbb66c5f27396ba84a0
gitVictors <WinVictors@mail.ru>
2025-09-08 21:43:37 +0300

    транспотрный справочник builder json

--- sprint12/Tems10_16lesson7_7/catalog_bld/json_builder.h
+++ sprint12/Tems10_16lesson7_7/catalog_bld/json_builder.h
@@ -3,46 +3,6 @@
 
 namespace json {
 
-class Builder;
-class DictKeyContext;
-class ArrayItemContext;
-
-class BaseContext {
-public:
-    BaseContext(Builder& builder) : builder_(builder) {}
-
-protected:
-    Builder& builder_;
-};
-
-class DictItemContext : public BaseContext {
-public:
-    DictItemContext(Builder& builder) : BaseContext(builder) {}
-
-    DictKeyContext  Key(const std::string& key);
-    Builder& EndDict();
-};
-
-class DictKeyContext : public BaseContext {
-public:
-    DictKeyContext(Builder& builder) : BaseContext(builder) {}
-
-    DictItemContext Value(Node::Value value);
-    DictItemContext StartDict();
-    ArrayItemContext StartArray();
-};
-
-class ArrayItemContext : public BaseContext {
-public:
-    ArrayItemContext(Builder& builder) : BaseContext(builder) {}
-
-    ArrayItemContext Value(Node::Value value);
-    Builder& EndArray();
-    // Методы для вложенных структур в массиве
-    DictItemContext StartDict();
-    ArrayItemContext StartArray();
-};
-
 
 
 class Builder {
@@ -62,7 +22,58 @@ public :
 
 
 private:
-   // void AddObject(Node::Value&& value, bool one_shot) ;
+
+
+    // Дружественные классы для доступа к приватным методам
+
+    class BaseContext {
+    public:
+        BaseContext(Builder& builder) : builder_(builder) {}
+
+        // Все методы Builder
+        DictKeyContext Key(const std::string& key);
+        BaseContext Value(Node::Value value);
+        DictItemContext StartDict();
+        ArrayItemContext StartArray();
+        Builder& EndDict();
+        Builder& EndArray();
+
+    protected:
+        Builder& builder_;
+    }
+
+    class DictItemContext : public BaseContext {
+    public:
+        using BaseContext::BaseContext;
+        DictKeyContext Key(const std::string& key);
+        Builder& EndDict();
+
+        // Запрещаем недопустимые
+        BaseContext Value(Node::Value) = delete;
+        ArrayItemContext StartArray() = delete;
+    };
+
+    class DictKeyContext : public BaseContext {
+    public:
+        DictKeyContext(Builder& builder) : BaseContext(builder) {}
+
+        DictItemContext Value(Node::Value value);
+        DictItemContext StartDict();
+        ArrayItemContext StartArray();
+    };
+
+    class ArrayItemContext : public BaseContext {
+    public:
+        ArrayItemContext(Builder& builder) : BaseContext(builder) {}
+
+        ArrayItemContext Value(Node::Value value);
+        Builder& EndArray();
+        // Методы для вложенных структур в массиве
+        DictItemContext StartDict();
+        ArrayItemContext StartArray();
+    };
+
+    // void AddObject(Node::Value&& value, bool one_shot) ;
     void AddNode(Node&& node, bool one_shot);
     Node::Value& GetCurrentValue() ;
 
@@ -70,12 +81,6 @@ private:
     std::vector<Node*> nodes_stack_;
     std::string current_key_;
     bool key_expected_ = false; // Ожидается ли ключ в словаре
-
-    // Дружественные классы для доступа к приватным методам
-    class DictKeyContext;
-    class DictItemContext;
-    class  ArrayItemContext;
-
 
 };
 
