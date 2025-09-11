fcb482a25bf7639ec55d354582e5389a5032f51b
sv <zuuuuk@mail.ru>
2025-09-11 17:31:48 +0300

    программа ведения статистики прочтения книги

--- sprint13/tems1_5lesson4_9/reader_stat/main.cpp
+++ sprint13/tems1_5lesson4_9/reader_stat/main.cpp
@@ -1,7 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+class ReadingManager {
+public:
+    ReadingManager()
+        : user_page_counts_(MAX_USER_COUNT_ + 1, -1)  // -1 значит, что не случилось ни одного READ
+        , page_achieved_by_count_(MAX_PAGE_COUNT_ + 1, 0) {
+    }
+
+    void Read(int user_id, int page_count){
+        for (int i = user_page_counts_[user_id] + 1; i <= page_count; ++i){
+            ++page_achieved_by_count_[i];
+        }
+
+        user_page_counts_[user_id] = page_count;
+
+    }
+
+    double Cheer(int user_id) {
+        if (user_page_counts_[user_id] == -1) return 0; // пользователь не читал
+
+        int total_users = 0;
+        int users_with_less_pages = 0;
+
+        // Подсчитываем общее количество пользователей, которые что-то читали
+        for (int i = 0; i <= MAX_USER_COUNT_; ++i) {
+            if (user_page_counts_[i] != -1) {
+                total_users++;
+                if (user_page_counts_[i] < user_page_counts_[user_id]) {
+                    users_with_less_pages++;
+                }
+            }
+        }
+
+        if (total_users == 1) return 1; // только текущий пользователь
+        return static_cast<double>(users_with_less_pages) / (total_users - 1);
+    }
+
+private:
+    static const int MAX_USER_COUNT_ = 100'000;
+    static const int MAX_PAGE_COUNT_ = 1'000;
+
+    std::vector<int> user_page_counts_;
+    std::vector<int> page_achieved_by_count_;
+};
 
 
 int main()
 {
+    ReadingManager readManager;
+    int count_cmd = 0;
+    cin >> count_cmd;
+
+    for (int i=  0; i < count_cmd; ++i) {
+        string cmd;
+        int id;
+
+        cin >> cmd >> id;
+
+        if (cmd == "READ"){
+            int page_count = 0;
+            cin >> page_count;
+            readManager.Read(id, page_count);
+        }
+        else if ( cmd == "CHEER" ){
+             cout << setprecision(6) << readManager.Cheer(id) << "\n";
+        }
+    }
 
     return 0;
 }
