7b4aa14be9cc69aedc4b781509a2d8e82d40bbdf
gitVictors <WinVictors@mail.ru>
2025-09-15 20:55:41 +0300

    merge разных контейнеров

--- sprint13/tems13_16lesson6_11/merge/main.cpp
+++ sprint13/tems13_16lesson6_11/merge/main.cpp
@@ -8,49 +8,68 @@ using namespace std;
 
 // Измените сигнатуру этой функции, чтобы она могла
 // сливать не только векторы, но и любые другие контейнеры
-template <typename T>
-void Merge(const vector<T>& src1, const vector<T>& src2, ostream& out) {
-    size_t i1 = 0, i2 = 0;
-    while (i1 < src1.size() && i2 < src2.size()) {
-        if (src1[i1] < src2[i2]) {
-            out << src1[i1++] << endl;
+// template <typename T>
+// void Merge(const vector<T>& src1, const vector<T>& src2, ostream& out) {
+//     size_t i1 = 0, i2 = 0;
+//     while (i1 < src1.size() && i2 < src2.size()) {
+//         if (src1[i1] < src2[i2]) {
+//             out << src1[i1++] << endl;
+//         } else {
+//             out << src2[i2++] << endl;
+//         }
+//     }
+//     while (i1 < src1.size()) {
+//         out << src1[i1++] << endl;
+//     }
+//     while (i2 < src2.size()) {
+//         out << src2[i2++] << endl;
+//     }
+// }
+
+template <typename iterator1, typename iterator2 >
+void Merge(iterator1 first1, iterator1 last1, iterator2 first2, iterator2 last2, ostream& out) {
+
+    while ( first1 != last1 && first2 != last2 ) {
+        if (std::less<>()(*first1, *first2)) {
+            out << *first1++ << endl;
         } else {
-            out << src2[i2++] << endl;
+            out << *first2++ << endl;
         }
     }
-    while (i1 < src1.size()) {
-        out << src1[i1++] << endl;
+    while ( first1 != last1 ) {
+        out << *first1++ << endl;
     }
-    while (i2 < src2.size()) {
-        out << src2[i2++] << endl;
+    while ( first2 != last2 ) {
+        out << *first2++ << endl;
     }
 }
 
 template <typename T>
 void MergeSomething(const vector<T>& src1, const vector<T>& src2, ostream& out) {
-    Merge(src1, src2, out);
+    //Merge(src1, src2, out);
+    Merge (src1.begin(), src1.end(), src2.begin(), src2.end(), out);
 }
 
 // Реализуйте эти функции. Они должны вызывать ваш вариант функции Merge.
 // Чтобы сократить кличество работы, можете реализовать вместо них одну шаблонную.
 template <typename T, typename S>
 void MergeSomething(const list<T>& src1, const vector<S>& src2, ostream& out) {
-    // ...
+    Merge (src1.begin(), src1.end(), src2.begin(), src2.end(), out);
 }
 
 template <typename T, typename S>
 void MergeSomething(const vector<T>& src1, const list<S>& src2, ostream& out) {
-    // ...
+    Merge (src1.begin(), src1.end(), src2.begin(), src2.end(), out);
 }
 
 template <typename S>
 void MergeSomething(const string_view& src1, const list<S>& src2, ostream& out) {
-    // ...
+    Merge (src1.begin(), src1.end(), src2.begin(), src2.end(), out);
 }
 
 template <typename T, typename S>
 void MergeSomething(const set<T>& src1, const vector<S>& src2, ostream& out) {
-    // ...
+    Merge (src1.begin(), src1.end(), src2.begin(), src2.end(), out);
 }
 
 // Реализуйте эту функцию:
@@ -59,6 +78,8 @@ void MergeHalves(const vector<T>& src, ostream& out) {
     size_t mid = (src.size() + 1) / 2;
     // Сделать Merge участка вектора от 0 до mid и от mid до конца.
     // Элемент с индексом mid включается во второй диапазон.
+    Merge(src.begin(), src.begin() + mid, src.begin() + mid, src.end(), out);
+
 }
 
 int main() {
