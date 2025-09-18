66dc81bcb3f6343e655310edebb2e9f192c2abe1
sv <zuuuuk@mail.ru>
2025-09-18 19:30:50 +0300

    менеджер бюджета

--- sprint13/tems13_16lesson3_7/maneger_many/budget_manager.cpp
+++ sprint13/tems13_16lesson3_7/maneger_many/budget_manager.cpp
@@ -7,6 +7,7 @@ const Date BudgetManager::START_DATE = Date("2000-01-01"s);
 const Date BudgetManager::END_DATE = Date("2100-01-01"s);
 
 double BudgetManager::ComputeTotalIncome(const Date& startDate, const Date& endDate) const {
+
     double totalIncome = 0;
 
     for (int i = GetDayIndex(startDate); i <= GetDayIndex(endDate); ++i) {
