7bf9dc1d567529f3c308f3441896f1c3d728d75e
sv <zuuuuk@mail.ru>
2025-09-18 19:22:32 +0300

    менеджер бюджета

--- sprint13/tems13_16lesson3_7/maneger_many/budget_manager.cpp
+++ sprint13/tems13_16lesson3_7/maneger_many/budget_manager.cpp
@@ -5,3 +5,13 @@ using namespace std::literals;
 
 const Date BudgetManager::START_DATE = Date("2000-01-01"s);
 const Date BudgetManager::END_DATE = Date("2100-01-01"s);
+
+double BudgetManager::ComputeTotalIncome(const Date& startDate, const Date& endDate) const {
+    double totalIncome = 0;
+
+    for (int i = GetDayIndex(startDate); i <= GetDayIndex(endDate); ++i) {
+        totalIncome += days_state_[i].income;
+    }
+
+    return totalIncome;
+}
--- sprint13/tems13_16lesson3_7/maneger_many/budget_manager.h
+++ sprint13/tems13_16lesson3_7/maneger_many/budget_manager.h
@@ -2,10 +2,25 @@
 
 #include "date.h"
 
+inline static const double TAX = 0.87;
+
+struct DayState {
+    double income = 0;
+};
+
+
 class BudgetManager {
 public:
     static const Date START_DATE;
     static const Date END_DATE;
 
     // Разработайте класс BudgetManager.
+    BudgetManager() {
+        days_state_ = std::vector<DayState>(GetDayIndex(END_DATE));
+    }
+
+    double ComputeTotalIncome(const Date& startDate, const Date& endDate) const;
+
+private:
+    std::vector<DayState> days_state_;
 };
