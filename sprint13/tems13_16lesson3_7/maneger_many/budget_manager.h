2dff9a79202be3e2cf714a2712649ff461ae5c41
sv <zuuuuk@mail.ru>
2025-09-18 10:28:33 +0300

    менеджер бюджета

---
+++ sprint13/tems13_16lesson3_7/maneger_many/budget_manager.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "date.h"
+
+class BudgetManager {
+public:
+    static const Date START_DATE;
+    static const Date END_DATE;
+
+    // Разработайте класс BudgetManager.
+};
