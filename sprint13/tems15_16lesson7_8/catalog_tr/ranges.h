1bdd9b8d28c136d3730899bd747cbc938a46b76e
sv <zuuuuk@mail.ru>
2025-09-24 11:13:05 +0300

    транспортный справочник подготовка к ревью

--- sprint13/tems15_16lesson7_8/catalog_tr/ranges.h
+++ sprint13/tems15_16lesson7_8/catalog_tr/ranges.h
@@ -1,4 +1,36 @@
-#ifndef RANGES_H
-#define RANGES_H
+#pragma once
 
-#endif // RANGES_H
+#include <iterator>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+
+namespace ranges {
+
+template <typename It>
+class Range {
+public:
+    using ValueType = typename std::iterator_traits<It>::value_type;
+
+    Range(It begin, It end)
+        : begin_(begin)
+        , end_(end) {
+    }
+    It begin() const {
+        return begin_;
+    }
+    It end() const {
+        return end_;
+    }
+
+private:
+    It begin_;
+    It end_;
+};
+
+template <typename C>
+auto AsRange(const C& container) {
+    return Range{container.begin(), container.end()};
+}
+
+}  // namespace rangesroute
